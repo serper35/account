ded2f750e9ba1e8537ab80b8b9c5ce714d79abea
serper35 <serper.wolf@gmail.com>
2024-08-02 04:32:53 +0300

    v0.11

--- check/src/main/java/com/hesfintech/check/controller/AuthenticationController.java
+++ check/src/main/java/com/hesfintech/check/controller/AuthenticationController.java
@@ -2,15 +2,11 @@ package com.hesfintech.check.controller;
 
 import com.hesfintech.check.model.User;
 import com.hesfintech.check.service.UserService;
-import com.hesfintech.check.util.AuthenticationResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
